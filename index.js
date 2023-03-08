6f4bc4fadc35cf9023fe8677320e65f5a9389b9c
felixNyalenda <felixasejunior@gmail.com>
2023-03-09 01:34:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+   const leftNumbers = dodger.style.left.replace("px", "");
+   const left = parseInt(leftNumbers, 10);
+
+   if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+   }
+}
+
+function moveDodgerRight() {
+   const leftNumbers = dodger.style.left.replace("px", "");
+   const left = parseInt(leftNumbers, 10);
+
+   if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+   }
+}
+
+document.addEventListener("keydown", (e) => {
+   if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+   } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+   }
+});
+// function (event) {
+//    if (event.key === "ArrowLeft") {
+//       const leftNumbers = dodger.style.left.replace("px", "");
+//       const left = parseInt(leftNumbers, 10);
+
+//       dodger.style.left = `${left - 1}px`;
+//    }
+// }
